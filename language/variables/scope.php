dadd7b9be79106e844053a5a7a63019c820b620e
nanketaishou <yintx_129@163.com>
2021-10-15 23:01:39 +0800

    php_docs scope

--- language/variables/scope.php
+++ language/variables/scope.php
@@ -1,5 +1,8 @@
 <?php
 
+/*
+ * scope
+ */
 //$a = 1; /* global scope */
 //function Test() {
 //    echo $a; /* reference to local scope variable */
@@ -7,12 +10,42 @@
 //Test();
 
 
-$a = 1;
-$b = 2;
-function Sum() {
-    global $a, $b;
-    $b = $a + $b;
-}
-Sum();
-echo $b;
+/*
+ * global
+ */
+//$a = 1;
+//$b = 2;
+//function Sum() {
+//    global $a, $b; //只是为了传参
+//    $b = $a + $b;
+//}
+//Sum();
+//echo $b;
 
+
+/*
+ * $GLOBALS[index]
+ */
+//$name = 'maple';
+//$age = 24;
+//$city = 'beijing';
+//
+//function index(){
+//    echo 'name:'.$GLOBALS['name'].'; age:'.$GLOBALS['age'].'; city:'.$GLOBALS['city'];
+//    var_dump($GLOBALS);
+//}
+//index();
+
+
+/*
+ * static
+ */
+function test_static() {
+    static $a = 0;
+    echo $a;
+    $a++;
+}
+test_static();
+test_static();
+test_static();
+test_static();
