c50be4ea4b641c09e37fefe7493a0cbb3dac8201
nanketaishou <yintx_129@163.com>
2021-11-12 02:13:09 +0800

    php_docs 匿名函数

--- language/functions/anonymous.php
+++ language/functions/anonymous.php
@@ -7,7 +7,7 @@
 function demo() {
     echo '我是普通函数' . PHP_EOL;
 }
-demo();
+//demo();
 
 // 匿名函数
 //function (){
@@ -20,4 +20,51 @@ $test = function () {
     echo '我是匿名函数';
 };
 // 需要配合变量函数调用，给匿名函数赋值，变量就有名字了
-$test();
+//$test();
+
+/**
+ * 闭包函数
+ * 使用use关键字，使用父函数的变量
+ */
+$var = '这是一个全局变量';
+
+//function father() {
+//    // 父函数的一个局部变量
+//    $var = '这是一个父函数的局部变量';
+//    function son() {
+//        global $var;
+//        echo $var;
+//    }
+//    son();
+//}
+//father();
+
+// 使用匿名函数的方式实现闭包
+function father(){
+    $var = '这是一个父函数的局部变量';
+    $son = function () use($var){
+        echo $var;
+    };
+    $son();
+}
+//father();
+
+
+/*
+ * https://www.php.net/manual/zh/functions.anonymous.php
+ */
+$message = 'hello';
+
+// 没有use
+$example = function () {
+    var_dump($message);
+};
+$example();
+
+// 继承 $message
+$example = function () use ($message) {
+    var_dump($message);
+};
+$example();
+
+
