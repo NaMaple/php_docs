c8ff0a72fd6adfe429cc25de2a6ccb6d01ba7e76
nanketaishou <yintx_129@163.com>
2021-11-12 14:53:11 +0800

    php_docs 抛出异常

---
+++ language/exceptions/throw_exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 抛出异常
+ */
+
+function getException(){
+    if (1<2) {
+        throw new Exception('throw exception');
+    }
+}
+
+try {
+    getException();
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+}
+
