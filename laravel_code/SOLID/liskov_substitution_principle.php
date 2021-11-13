60d250bb020a117271d4a36444e4dffe90b486e2
yintianxiong <yintx_129@163.com>
2021-11-14 01:53:17 +0800

    设计原则 里氏替换原则

--- laravel_code/SOLID/liskov_substitution_principle.php
+++ laravel_code/SOLID/liskov_substitution_principle.php
@@ -5,5 +5,28 @@
  * 里氏替换原则（LSP）一个抽象的任意实现都可以在声明该抽象的地方替换它
  *
  * 子类在继承父类的时候，尽量不要重写父类的方法
- *
  */
+
+class Product
+{
+    public function getPrice()
+    {
+        echo '返回产品的价格' . PHP_EOL;
+    }
+}
+
+class SubProduct extends Product
+{
+    public function getPrice()
+    {
+        // 重写了父类的方法，违背了里氏替换原则
+//        parent::getPrice();
+        echo '返回订单的价格' . PHP_EOL;
+    }
+}
+
+$obj = new Product();
+$obj->getPrice(); // 返回产品的价格
+
+$obj = new SubProduct();
+$obj->getPrice(); // 返回订单的价格
