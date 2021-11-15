e9daaac822b8216dae19db525f02bcb10d7942fc
yintianxiong <yintx_129@163.com>
2021-11-15 10:53:20 +0800

    vendor 自动加载源码分析

---
+++ laravel_preknowledge/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ee40fe698465e92f89a581b7f6112837d5dfb915',
+    'name' => 'yintianxiong/laravel_preknowledge',
+  ),
+  'versions' => 
+  array (
+    'yintianxiong/laravel_preknowledge' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ee40fe698465e92f89a581b7f6112837d5dfb915',
+    ),
+  ),
+);
